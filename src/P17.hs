166ed1062b159ff9dc79229d58bf12309203ada8
pyuk <psi.razputin@gmail.com>
2017-03-10 16:20:15 -0600

    shortened 17

--- src/P17.hs
+++ src/P17.hs
@@ -4,35 +4,27 @@ module P17 (
 numWords :: [String]
 numWords =["one","two","three","four","five","six","seven","eight","nine",
            "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen",
-           "seventeen","eighteen","nineteen"]
+           "seventeen","eighteen","nineteen","twenty","thirty","forty","fifty",
+           "sixty","seventy","eighty","ninety"]
 
 displayNum :: Int -> String
 displayNum x | x < 20 = displayOnes x
-             | x >= 20 && x < 100 = concat $ displayTens x
-             | x >= 100 && x < 1000 = concat  $ displayHundreds' x
+             | x >= 20 && x < 100 = displayTens' x
+             | x >= 100 && x < 1000 = displayHundreds' x
              | otherwise = "onethousand"
 
 displayOnes :: Int -> String
 displayOnes x = numWords !! (x - 1)
 
-displayTens :: Int -> [String]
-displayTens 0 = []
-displayTens x
-  | x < 20 = displayOnes x : []
-  | x >= 20 && x < 30 = "twenty" : displayTens (x `mod` 10) 
-  | x >= 30 && x < 40 = "thirty" : displayTens (x `mod` 10)
-  | x >= 40 && x < 50 = "forty" : displayTens (x `mod` 10)
-  | x >= 50 && x < 60 = "fifty" : displayTens (x `mod` 10)
-  | x >= 60 && x < 70 = "sixty" : displayTens (x `mod` 10)
-  | x >= 70 && x < 80 = "seventy" : displayTens (x `mod` 10)
-  | x >= 80 && x < 90 = "eighty" : displayTens (x `mod` 10)
-  | x >= 90 && x < 100 = "ninety" : displayTens (x `mod` 10)
-  | otherwise = []
+displayTens' :: Int -> String
+displayTens' x | x `mod` 10 == 0 = numWords !! (19 + (x `div` 10 - 2))
+               | otherwise = numWords !! (19 + (x `div` 10 - 2)) ++
+                 displayOnes (x `mod` 10)
 
-displayHundreds' :: Int -> [String]
-displayHundreds' x | x >= 100 = displayOnes (x `div` 100) : "hundred" :
+displayHundreds' :: Int -> String
+displayHundreds' x | x >= 100 = displayOnes (x `div` 100) ++ "hundred" ++
                      displayHundreds' (x `mod` 100)
-                   | x < 100 && x/= 0 = "and" : displayTens x
+                   | x < 100 && x/= 0 = "and" ++ displayTens' x
                    | otherwise = []
 
 p17 :: Int
